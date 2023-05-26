b91ffeaae6e02a7e9d60fb4a64b6bf1058a6bb40
jeremie.delbruel <jeremie.delbruel@gmail.com>
2023-05-26 14:33:10 +0200

    first commit

---
+++ Assets/Mesh_Controller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+public class Mesh_Controller : MonoBehaviour
+{
+    [Range(1.5f, 5f)]
+    public float radius = 2f;
+    [Range(1.5f, 5f)]
+    public float deformationStrength = 2f;
+
+    private Mesh mesh;
+    private Vector3[] vertices, modifiedVerts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        modifiedVerts = mesh.vertices;
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    // Update is called once per frame
+    void RecalculateMesh()
+    {
+        mesh.vertices = modifiedVerts;
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+        mesh.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            for (int v = 0; v < modifiedVerts.Length; v++)
+            {
+                Vector3 distance = modifiedVerts[v] - hit.point;
+
+                float smoothingFactor = 2f;
+                float force = (deformationStrength / (1f + hit.point.sqrMagnitude)) * 1000;
+
+                if (distance.sqrMagnitude < radius)
+                {
+                    if (Input.GetMouseButton(0))
+                    {
+                        modifiedVerts[v] = modifiedVerts[v] + (Vector3.up * force * 5) / smoothingFactor;
+                    }
+                    else if (Input.GetMouseButton(1))
+                    {
+                        modifiedVerts[v] = modifiedVerts[v] + (Vector3.down * force * 5) / smoothingFactor;
+                    }
+                }
+            }
+        }
+        RecalculateMesh();
+    }
+
+}
---
+++ Assets/Plane.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Plane : MonoBehaviour
+{
+    public int res;
+    int size;
+    public int taille;
+    private Mesh m;
+    private Vector3[] vertices;
+    private int[] triangles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        res = res - 1;
+        m = new Mesh();
+        GetComponent<MeshFilter>().mesh = m;
+
+        vertices = new Vector3[(res +1) * (res +1)];
+        int i = 0;
+        for(int z = 0; z <= res; z++)
+        {
+            for (int x = 0; x <= res; x++)
+            {
+                vertices[i] = new Vector3(x * taille / res, 0, z * taille / res );
+                i++;
+            }
+        }
+
+        //triangles = new int[3 * (int)(2 * Mathf.Pow(res, 2))];
+        triangles = new int[6 * res * res];
+        int vert = 0;
+        int tris = 0;
+        
+        Debug.Log(vertices.Length);
+        Debug.Log(triangles.Length);
+        for (int z = 0; z < res ; z++)
+        {
+            for (int x = 0; x < res ; x++)
+            {
+                triangles[tris + 0] = vert;
+                triangles[tris + 1] = vert + res + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + res + 1;
+                triangles[tris + 5] = vert + res + 2;
+
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+        m.vertices = vertices;
+        m.triangles = triangles;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
